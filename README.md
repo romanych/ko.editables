7b80c9f2fea59dc6e192d5439c1aad4e2c2d4ac7
Roman Gomolko <rgomolko@gmail.com>
2012-01-23 00:14:08 +0200

    Initial

---
+++ README.md
@@ -0,0 +1,73 @@
+﻿ko.editables
+============
+This is plugin for KnockoutJS which allows to accept or rollback changes on observables or view models.
+It is extremely easy to use solution suitable in most use cases.
+
+editable extender
+-----------------
+
+Adds following methods to any writeable observable:
+
+* `beginEdit()`
+* `commit()`
+* `rollback() `
+
+Also it adds `hasChanges` observable property.
+`hasChanges` returns boolean value indicating wether the actual value of observable is different to initial value or not.
+Under initial value we understand value of ubservable when `beginEdit()` was called.
+
+Example: 
+
+    var name = ko.observable().extend({editable: true});
+    var nameLength = ko.dependentObservable(function() {
+        return name() ? name().length : 0;
+    });
+
+    name('user');       // name set to 'user'
+    name.beginEdit();   // begin transaciton
+    name('me');         // name set to 'me', nameLength was recalculated
+    nameLength();       // gives us 2
+    hasChanges();       // gives us `true`
+    name.commit();      // transaction commited; values are unchanged since last edit; we could start another one
+    hasChanges();       // gives us `false`
+    name.rollback();    // nothing happens since transation is commited
+    hasChanges();       // gives us `false`
+    name.beginEdit();   // begin another transaction
+    name('someone');    // name set to 'someone', nameLength was recalculated
+    name.rollback();    // rollback transaction; name set to initial value 'me', nameLength recalculated
+    name();             // gives us 'me'
+
+*Note*: it is safe to extend same observable multiple times
+
+ko.editable plugin
+------------------
+
+Extends any object with same methods and properties as editable extender.
+Basically it iterates over all poperties of passed object and extends all writeable observables with editable extender.
+Also it goes into deep when scaning for observables. So you could use pretty compex viewModels and activate editable with single call
+
+Example:
+
+    var user = {
+        FirstName: ko.observable('Some'),
+        LastName: ko.observable('Person'),
+        Address: {
+            Country: ko.observable('USA'),
+            City: ko.observable('Washington')
+        }
+    };
+    ko.editable(user);
+
+    user.beginEdit();
+    user.FirstName('MyName');
+    user.hasChanges();          // returns `true`
+    user.commit();
+    user.hasChanges();          // returns `false`
+    user.Address.Country('Ukraine');
+    user.hasChanges();          // returns `true`
+    user.rollback();
+    user.Address.Country();     // returns 'USA'
+
+*Tip*: if you are using view models based on classes the best practice is to call
+`ko.editable(this)` when all data properties are defined
+
---
+++ ko.editables.js
@@ -0,0 +1,117 @@
+﻿// editable extender and ko.editable plugin for viewModels
+// (c) Roman Gomolko - rgomolko@gmail.com
+// License: MIT (http://www.opensource.org/licenses/mit-license.php)
+
+/*
+Exports: 
+    ko.extenders.editable: ko.observable().extend({editable: true|false});
+    ko.editable: ko.editable(objectWithObservables, boolean autoInit);
+
+    Editable adds following methods into observables and objects:
+        - beginEdit()
+        - commit()
+        - rollback()
+        - hasChanges() - observable
+*/
+
+(function (ko, undefined) {
+    var deferredDependentObservable = function (readFunction) {
+        return ko.dependentObservable({ read: readFunction, deferEvaluation: true });
+    };
+
+    ko.extenders['editable'] = function (target, enabledEditable) {
+        enabledEditable = enabledEditable === undefined ? true : enabledEditable;
+        // Protect from double initialization
+        if (target.hasOwnProperty('editable')) {
+            return target;
+        }
+        target.editable = enabledEditable;
+
+        var oldValue;
+        var inTransaction = ko.observable(false);
+
+        target.beginEdit = function () {
+            var currentValue = target();
+            if (currentValue instanceof Array) {
+                currentValue = currentValue.slice(); // make copy
+            }
+            oldValue = currentValue;
+            inTransaction(true);
+        };
+
+        target.commit = function () {
+            inTransaction(false);
+        };
+
+        target.rollback = function () {
+            if (inTransaction()) {
+                target(oldValue);
+            }
+        };
+
+        target.hasChanges = deferredDependentObservable(function () {
+            var hasChanges = inTransaction() && oldValue != target();
+            return hasChanges;
+        });
+
+        return target;
+    };
+
+    ko.editable = function (viewModel, autoInit) {
+        if (typeof (viewModel.beginEdit) == 'function') {
+            return;
+        }
+
+        autoInit = autoInit === undefined ? true : autoInit;
+        var editables = ko.observableArray();
+
+        viewModel.beginEdit = function () {
+            ko.utils.arrayForEach(editables(), function (obj) {
+                obj.beginEdit();
+            });
+        };
+
+        viewModel.commit = function () {
+            ko.utils.arrayForEach(editables(), function (obj) {
+                obj.commit();
+            });
+        };
+
+        viewModel.rollback = function () {
+            ko.utils.arrayForEach(editables(), function (obj) {
+                obj.rollback();
+            });
+        };
+
+        viewModel.addEditable = function (editable) {
+            editables.push(editable.extend({ editable: true }));
+        };
+
+        viewModel.hasChanges = deferredDependentObservable(function () {
+            var editableWithChanges = ko.utils.arrayFirst(editables(), function (editable) {
+                return editable.hasChanges();
+            });
+            return editableWithChanges != null;
+        });
+
+        if (autoInit) {
+            (function makeEditable(rootObject) {
+                for (var propertyName in rootObject) {
+                    var property = rootObject[propertyName];
+                    if (ko.isWriteableObservable(property)) {
+                        var observable = property;
+                        observable.extend({ editable: true });
+                        // Allow to skip observables extended with { editable: false } arguments
+                        if (observable.editable) {
+                            editables.push(observable);
+                        }
+                    }
+                    property = ko.utils.unwrapObservable(property);
+                    if (typeof (property) == 'object') {
+                        makeEditable(property);
+                    }
+                }
+            })(viewModel);
+        }
+    };
+})(ko);
